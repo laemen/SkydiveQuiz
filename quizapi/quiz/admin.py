2afc6ad6b3eb6236aff3b169a63711554e4bf2e5
Laemen, Frans <frans.laemen@kpn.com>
2020-04-03 14:29:39 +0200

    cleanup

--- quizapi/quiz/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from quiz.models import Answer, Profile, Question
-
-admin.site.register(Answer)
-admin.site.register(Profile)
-admin.site.register(Question)
--- quizapi/quiz/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuizConfig(AppConfig):
-    name = 'quiz'
-
-    def ready(self):
-        import quiz.signals
